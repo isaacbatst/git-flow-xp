cf8f2595e90d855119a8f9d8f0e76b1e0e78be30
Isaac Batista <isaacbatista@rits.com.br>
2020-02-13 11:09:03 -0300

    add bye route

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ server.get('/', (req, res) => {
   res.send('Testing git, first feature');
 })
 
+server.get('/bye', (req, res) => {
+  res.send('Bye, world!?');
+})
 server.get('/hello', (req, res) => {
   res.send('Hello, is it me yr looking for?')
 })

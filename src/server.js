6115d8af0ea12c9b4b5fc59074d4f518b41e60d3
Isaac Batista <isaacbatista@rits.com.br>
2020-02-13 11:11:18 -0300

    add hello route

--- src/server.js
+++ src/server.js
@@ -7,3 +7,7 @@ server.listen(3000);
 server.get('/', (req, res) => {
   res.send('Testing git, first feature');
 })
+
+server.get('/hello', (req, res) => {
+  res.send('Hello, is it me yr looking for?')
+})
